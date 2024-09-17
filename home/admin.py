915a18eeb8db70b926ac2b11c7ab6b5f40073237
juanmbermeo <bermeojuanmanuel1@gmail.com>
2024-09-17 16:10:57 -0500

    first commit

---
+++ home/admin.py
@@ -0,0 +1,104 @@
+from django.contrib import admin
+from .models import Evento, Usuario, ServicioMunicipal, Alerta, Basura, Comunicado, ContactoEmergencia, PagoServicio, Barrio, TipoBasura
+from unfold.admin import ModelAdmin
+
+@admin.register(Evento)
+class EventoAdmin(ModelAdmin):
+    list_display = ('titulo', 'fecha_evento', 'hora_evento', 'ubicacion')
+    search_fields = ('titulo',)
+    list_filter = ('fecha_evento',)
+    ordering = ('titulo',)  # Ordenar por fecha de evento de forma descendente
+    readonly_fields = ('likes',)
+    fieldsets = (
+        ('Información básica', {'fields': ('titulo', 'descripcion')}),
+        ('Detalles del evento', {'fields': ('fecha_evento', 'hora_evento', 'ubicacion')}),
+        ('Multimedia', {'fields': ('imagen', 'likes')}),
+    )
+
+
+@admin.register(Usuario)
+class UsuarioAdmin(ModelAdmin):
+    list_display = ('username', 'email', 'telefono', 'date_joined')
+    search_fields = ('username', 'email')
+    list_filter = ('username', 'email')
+    fieldsets = (
+        ('Información básica', {'fields': ('username', 'email', 'password')}),
+        ('Información adicional', {'fields': ('telefono', 'avatar')}),
+    )
+
+
+@admin.register(ServicioMunicipal)
+class ServicioMunicipalAdmin(ModelAdmin):
+    list_display = ('nombre', 'ubicacion', 'contacto', 'hora_apertura', 'hora_cierre')
+    search_fields = ('nombre',)
+    list_filter = ('hora_apertura',)
+    ordering = ('nombre',)
+    readonly_fields = ('id_servicio',)
+    fieldsets = (
+        ('Información básica', {'fields': ('nombre', 'descripcion')}),
+        ('Contacto', {'fields': ('ubicacion', 'contacto', 'email')}),
+        ('Horario', {'fields': ('hora_apertura', 'hora_cierre')}),
+    )
+
+
+@admin.register(Alerta)
+class AlertaAdmin(ModelAdmin):
+    list_display = ('titulo', 'fecha', 'tipo_alerta', 'estado')
+    search_fields = ('titulo',)
+    list_filter = ('tipo_alerta', 'estado')
+    ordering = ('titulo',) 
+    readonly_fields = ('fecha',)
+    fieldsets = (
+        ('Información básica', {'fields': ('titulo', 'descripcion')}),
+        ('Detalles de la alerta', {'fields': ('fecha', 'ubicacion', 'tipo_alerta')}),
+        ('Multimedia', {'fields': ('imagenes',)})
+    )
+
+
+@admin.register(PagoServicio)
+class PagoServicioAdmin(ModelAdmin):
+    list_display = ('nombre_servicio', 'icono', 'link',)
+    search_fields = ('nombre_servicio',)
+    list_filter = ('nombre_servicio',)
+ 
+    
+@admin.register(Comunicado)
+class ComunicadoAdmin(ModelAdmin):
+    list_display = ('titulo', 'fecha_publicacion', 'estado', 'likes', 'compartidos')
+    search_fields = ('titulo',)
+    list_filter = ('estado', 'titulo')
+    readonly_fields = ('fecha_publicacion', 'likes', 'compartidos')
+    fieldsets = (
+        ('Información básica', {'fields': ('titulo', 'texto')}),
+        ('Detalles', {'fields': ('imagen', 'fecha_publicacion', 'estado')}),
+        ('Estadísticas', {'fields': ('likes', 'compartidos')}),
+    )
+    
+
+@admin.register(ContactoEmergencia)
+class ContactoEmergenciaAdmin(ModelAdmin):
+    list_display = ('nombre', 'telefono', 'icono')
+    search_fields = ('nombre',)
+    fieldsets = (
+        ('Información básica', {'fields': ('nombre', 'telefono')}),
+        ('Imagen', {'fields': ('icono',)})
+    )
+      
+
+@admin.register(Barrio)
+class BarrioAdmin(ModelAdmin):
+    list_display = ('nombre',)
+    search_fields = ('nombre',)
+
+
+@admin.register(TipoBasura)
+class TipoBasuraAdmin(ModelAdmin):
+    list_display = ('nombre',)
+    search_fields = ('nombre',)
+
+
+@admin.register(Basura)
+class RecoleccionBasuraAdmin(ModelAdmin):
+    list_display = ('barrio', 'fecha', 'hora', 'tipo_basura')
+    list_filter = ('barrio', 'fecha', 'tipo_basura')
+    search_fields = ('barrio__nombre',)
---
+++ home/models.py
@@ -0,0 +1,156 @@
+from django.db import models
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator, EmailValidator
+
+import re
+
+def validar_telefono(value):
+    # Expresión regular para validar el formato de teléfono (ajusta según tu formato)
+    regex = r'^\+\d{1,3}\s\d{10}$'  # Ejemplo: +57 3112345678
+    if not re.match(regex, value):
+        raise ValidationError(('El número de teléfono debe tener el formato +XXX XXXXXXXXX.'), params={'value': value})
+
+class Usuario(AbstractUser):
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='usuarios',  # Nombre único para esta relación
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='usuarios',  # Nombre único para esta relación
+        blank=True
+    )
+    telefono = models.CharField(max_length=15, unique=True, validators=[validar_telefono])
+    email = models.EmailField(unique=True, validators=[EmailValidator()],
+                             error_messages={'unique': "Ya existe un usuario con ese correo electrónico."})
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+class ServicioMunicipal(models.Model):
+    id_servicio = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100, null=False,)
+    descripcion = models.TextField()
+    ubicacion = models.CharField(max_length=255)
+    contacto = models.CharField(
+        max_length=20,
+        validators=[
+            RegexValidator(
+                regex=r'^\+\d{1,3}\s?\d{8,14}$',
+                message="El número de teléfono debe tener el formato +XXX XXXXXXXXX"
+            )
+        ]
+    )
+    email = models.EmailField(blank=True)   
+    hora_apertura = models.TimeField()
+    hora_cierre = models.TimeField()
+    icono = models.ImageField(upload_to='iconos/')  # Para almacenar iconos
+    
+    class Meta:
+        verbose_name_plural = 'Servicios Municipales'
+        verbose_name = 'Servicio Municipal'
+        ordering = ['nombre']
+
+class Evento(models.Model):
+    id_evento = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=150, null=False, blank=False)
+    descripcion = models.TextField()
+    fecha_evento = models.DateField()
+    hora_evento = models.TimeField()
+    ubicacion = models.CharField(max_length=255, blank=True, null=True)
+    imagen = models.ImageField(upload_to='eventos/', blank=True, null=True)
+    likes = models.PositiveIntegerField(default=0)
+    
+    def incrementar_likes(self):
+        self.likes += 1
+        self.save(update_fields=['likes'])
+        
+    def __str__(self):
+        return self.titulo
+
+class Alerta(models.Model):
+    id_alerta = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=150, null=False, blank=False)
+    descripcion = models.TextField()
+    fecha= models.DateTimeField(auto_now_add=True)
+    ubicacion = models.CharField(max_length=255, blank=True, null=True)
+    tipo_alerta = models.CharField(max_length=50, choices={'emergencia': 'Emergencia','informacion': 'Información',}, default='informacion')    
+    imagenes = models.FileField(upload_to='imagenes_alertas/', blank=True, null=True)  # Permite múltiples imágenes
+    estado = models.CharField(max_length=20, choices=[
+        ('activa', 'Activa'),
+        ('resuelta', 'Resuelta'),
+        ('cancelada', 'Cancelada'),
+    ], default="activa")
+    
+    def __str__(self):
+        return self.titulo
+        
+class PagoServicio(models.Model):
+    id_contacto = models.AutoField(primary_key=True)
+    nombre_servicio = models.CharField(max_length=100, null=False)
+    icono = models.ImageField(upload_to='iconos_recoleccion/')
+    link = models.URLField(validators=[URLValidator()])
+
+class Comunicado(models.Model):
+    id_comunicado = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+    imagen = models.ImageField(upload_to='imagenes_comunicados/')
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField(default=0)
+    compartidos = models.PositiveIntegerField(default=0)
+    estado = models.CharField(max_length=20, choices=[
+        ('activo', 'Activo'),
+        ('inactivo', 'Inactivo'),
+        ('borrado', 'Borrado'),
+    ], default='activo')
+
+    def incrementar_likes(self):
+        self.likes += 1
+        self.save(update_fields=['likes'])
+
+    def incrementar_compartidos(self):
+        self.compartidos += 1
+        self.save(update_fields=['compartidos'])
+
+    def save(self, *args, **kwargs):
+        # Si el objeto ya existe, no permitimos modificar likes y compartidos
+        if self.pk:
+            del self.__dict__['likes']
+            del self.__dict__['compartidos']
+        super().save(*args, **kwargs)
+
+    def clean(self):
+        if self.pk and (self.likes != self._original_likes or self.compartidos != self._original_compartidos):
+            raise ValidationError('No puedes modificar los campos likes y compartidos.')
+        
+    def __str__(self):
+        return self.titulo
+
+class ContactoEmergencia(models.Model):
+    id_contacto = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100, null=False)
+    telefono = models.CharField(max_length=15, null=False)
+    icono = models.ImageField(upload_to='iconos_emergencia/')
+    
+class Barrio(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class TipoBasura(models.Model):
+    nombre = models.CharField(max_length=50)
+
+class Basura(models.Model):
+    id_basura = models.AutoField(primary_key=True)
+    barrio = models.ForeignKey(Barrio, on_delete=models.CASCADE)
+    fecha = models.DateField()
+    hora = models.TimeField()
+    tipo_basura = models.ForeignKey(TipoBasura, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"Recogida de basura en el barrio {self.barrio} el {self.fecha} a las {self.hora}"
+    
+
